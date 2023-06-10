c3368b705371501af44d07f9c18b0126aa0daf4b
Jiri Belecky (jibeleck) <jibeleck@cisco.com>
2023-06-10 14:38:41 +0200

    fixes

--- kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/AudioPlayer.kt
+++ kotlin-audio/src/main/java/com/doublesymmetry/kotlinaudio/players/AudioPlayer.kt
@@ -4,5 +4,15 @@ import android.content.Context
 import com.doublesymmetry.kotlinaudio.models.BufferConfig
 import com.doublesymmetry.kotlinaudio.models.CacheConfig
 import com.doublesymmetry.kotlinaudio.models.PlayerConfig
+import android.support.v4.media.session.MediaSessionCompat
 
-class AudioPlayer(context: Context, playerConfig: PlayerConfig = PlayerConfig(), bufferConfig: BufferConfig? = null, cacheConfig: CacheConfig? = null): BaseAudioPlayer(context, playerConfig, bufferConfig, cacheConfig)
+class AudioPlayer(
+    context: Context,
+    playerConfig: PlayerConfig = PlayerConfig(),
+    bufferConfig: BufferConfig? = null,
+    cacheConfig: CacheConfig? = null,
+    mediaSession: MediaSessionCompat = MediaSessionCompat(
+        context,
+        "KotlinAudioPlayer"
+    )
+) : BaseAudioPlayer(context, playerConfig, bufferConfig, cacheConfig, mediaSession)
